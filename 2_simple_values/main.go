529930d3b8d400a070b69c42b43e682663221496
123MuhammadBilal <kkmuhammadbilal2@gmail.com>
2024-07-19 11:02:03 +0500

    2_simple_values

---
+++ 2_simple_values/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// simple values
+
+	//int
+	fmt.Println(1 + 1)
+
+	//strings
+	fmt.Println("hello golang")
+
+	//bool
+	fmt.Println(true)
+	fmt.Println(false)
+
+	//floots
+	fmt.Println(10.5)
+	fmt.Println(7.0 / 3.0)
+}
